872b4ba070426b2822ec0046afd8c65779f2a397
Daniel Barbieri <90072728+DHB1985@users.noreply.github.com>
2022-02-09 17:19:41 -0300

    Vid91 CSS Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,75 @@
 import React from "react";
-import { Button, Modal, Text } from "react-native";
+import {
+  Modal,
+  Text,
+  StyleSheet,
+  View,
+  TextInput,
+  ScrollView,
+} from "react-native";
+import { styles } from "./FormStyles";
 
 const Form = ({ modalVisible }) => {
   return (
     <Modal animationType="slide" visible={modalVisible}>
-   
-   
+      <View style={styles.content}>
+        <ScrollView>
+          <Text style={styles.title}>
+            Nueva {""}
+            <Text style={styles.titleBold}>Cita</Text>
+          </Text>
+
+          <View style={styles.inputContent}>
+            <Text style={styles.label}>Nombre Paciente</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="default"
+              placeholder="Nombre Paciente..."
+              placeholderTextColor={"#666"}
+            />
+          </View>
+
+          <View style={styles.inputContent}>
+            <Text style={styles.label}>Nombre Propietario</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="default"
+              placeholder="Nombre Propietario..."
+              placeholderTextColor={"#666"}
+            />
+          </View>
+
+          <View style={styles.inputContent}>
+            <Text style={styles.label}>e-mail Propietario</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="email-address"
+              placeholder="e-mail Propietario..."
+              placeholderTextColor={"#666"}
+            />
+          </View>
+
+          <View style={styles.inputContent}>
+            <Text style={styles.label}>Teléfono Propietario</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="number-pad"
+              placeholder="Teléfono Propietario..."
+              placeholderTextColor={"#666"}
+            />
+          </View>
+
+          <View style={styles.inputContent}>
+            <Text style={styles.label}>Síntomas</Text>
+            <TextInput
+              style={styles.input}
+              keyboardType="default"
+              placeholder="Síntomas Paciente..."
+              placeholderTextColor={"#666"}
+            />
+          </View>
+        </ScrollView>
+      </View>
     </Modal>
   );
 };
---
+++ src/components/FormStyles.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: "600",
+    textAlign: "center",
+    marginTop: 10,
+    color: "#FFF",
+  },
+  titleBold: {
+    fontWeight: "900",
+  },
+  content: {
+    backgroundColor: "#6D28D9",
+    flex: 1,
+  },
+  input: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+  },
+  label: {
+    color: "#FFF",
+    marginBottom: 10,
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  inputContent: {
+    marginTop: 10,
+    marginHorizontal: 30,
+  },
+});
