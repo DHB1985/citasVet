8311878dcc3a3867895c57d74e8b4268bbbb1b14
Daniel Barbieri <90072728+DHB1985@users.noreply.github.com>
2022-02-11 19:37:08 -0300

    Vid102 BTN edit and delete

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
 
-import {styles} from './PatientStyles.js'
+import { styles } from "./PatientStyles.js";
 
 const Patient = ({ item }) => {
   const { patient, date } = item;
@@ -23,6 +23,15 @@ const Patient = ({ item }) => {
       <Text style={styles.label}>Paciente: </Text>
       <Text style={styles.text}>{patient}</Text>
       <Text style={styles.date}>{dateFormat(date)}</Text>
+
+      <View style={styles.btnContainer}>
+        <Pressable style={[styles.btn, styles.btnEdit]}>
+          <Text style={styles.btnText}>Editar</Text>
+        </Pressable>
+        <Pressable style={[styles.btn, styles.btnDelete]}>
+          <Text style={styles.btnText}>Eliminar</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
--- src/components/PatientStyles.js
+++ src/components/PatientStyles.js
@@ -16,11 +16,33 @@ export const styles = StyleSheet.create({
     text:{
         color: '#6D28D9',
         fontSize: 24,
-        fontWeight: '100',
+        fontWeight: '700',
         marginBottom: 10,
     },
     date:{
         color: '#374151',
+    },
+    btnContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop:20,
+    },
+    btn:{
+        paddingVertical: 5,
+        paddingHorizontal:20,
+        borderRadius:5,
+    },
+    btnEdit:{
+        backgroundColor:'#F59E0B',
+    },
+    btnText:{
+        textTransform: 'uppercase',
+        fontWeight: '700',
+        fontSize: 12,
+        color: '#FFF',
+    },
+    btnDelete:{
+        backgroundColor: '#EF4444',
     }
 
 })
